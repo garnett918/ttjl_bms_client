cca20fbddc299d52b83e9b03cc004e550a665d29
garnett918 <garnett_918@126.com>
2017-12-27 20:41:18 +0800

    固定资产开发完成

--- admin/assetmanage/ctrl.js
+++ admin/assetmanage/ctrl.js
@@ -25,8 +25,29 @@ app.controller('AMFixedAssetManageController', function($scope, $resource,$state
         });
     }
     //搜索跳转
-    $scope.search = function(){
-        $state.go('app.assetmanage.fixedassetmanage',{search:$scope.search_context});
+    $scope.search = function(obj){
+        //$state.go('app.assetmanage.fixedassetmanage',{search:$scope.search_context});
+        //alert("$stateParams.page"+$stateParams.page);
+        //alert("$stateParams.search"+obj.search);
+        $scope.query(1,obj.search);
+    }
+    //分页跳转
+    $scope.paging = function(obj){
+        //$state.go('app.assetmanage.fixedassetmanage',{search:$scope.search_context});
+        //alert("$stateParams.page"+page.page);
+        //alert("$stateParams.search"+page.search);
+        $scope.query(obj.page,obj.search);
+    }
+    //分页跳转
+    $scope.setdbid = function(obj){
+        //$state.go('app.assetmanage.fixedassetmanage',{search:$scope.search_context});
+        //alert("$stateParams.page"+obj);
+        //alert("$stateParams.search"+obj.dbid);
+        //$scope.dbid_hidden = obj.dbid;
+        //$scope.search_context = obj.dbid;
+        angular.element("#dbidhidden").val(obj.dbid);
+        angular.element("#forgectrl").click();
+        angular.element("#propset").click();
     }
     //全选
     var selected = false;
@@ -39,6 +60,101 @@ app.controller('AMFixedAssetManageController', function($scope, $resource,$state
     //自定义操作处理，其中1为删除所选记录
     $scope.exec = function(){
 
+    }
+    //根据url参数（分页、搜索关键字）查询数据
+    $scope.query($stateParams.page,$stateParams.search);
+});
+
+
+app.controller('AMFAMPropertiesController', function($scope, $resource,$stateParams,$modal,$state) {
+    //查询
+    $scope.query = function(page,filter){
+    var prophidden =  angular.element("#prophidden").val();
+    //alert("prophidden  "+prophidden);
+        if(!page){
+            page=1;
+        }else{
+            page=parseInt(page);
+        }
+        var proparr=prophidden.split(','); 
+        if(proparr.length>1)
+        {
+            var PAGE_SIZE=4;
+            var limit = PAGE_SIZE;
+            var offset = PAGE_SIZE*(page-1);
+            var total_count = proparr.length-1;
+            var page_count = Math.floor(total_count/PAGE_SIZE);
+            if(total_count%PAGE_SIZE>0){
+                page_count++;
+            }
+
+            var data = {
+                total_count:total_count,
+                page_count:page_count,
+                next:page<page_count,
+                previous:page>1,
+                results:[],
+            }
+
+            data.page_index = page;
+            data.pages = [];    //页签表
+            var N = 4;          //每次显示5个页签
+            var s = Math.floor(page/N)*N;
+            if(s==page)s-=N;
+            s += 1;
+            var e = Math.min(data.page_count,s+N-1)
+            for(var i=s;i<=e;i++)
+                data.pages.push(i)
+
+            for(var i=offset;i<(offset+limit);i++)
+            {
+
+                if(i<proparr.length)
+                {
+                    var propitem = proparr[i];
+                     //alert(propitem);
+                    var propitemarr=propitem.split('###'); 
+                    //var propstr = '{value:'+propitemarr[0]+',name:'+propitemarr[1]+'}';
+                    var propstr = {value:propitemarr[1],name:propitemarr[0]};
+                    data.results.push(propstr); 
+                }
+            }
+            
+
+            // data.results = [{
+            //         value:1,
+            //         name:"灯箱"
+            //     },
+            //     {
+            //         value:2,
+            //         name:"灯箱1"
+            //     },{
+            //         value:3,
+            //         name:"灯箱2"
+            //     },{
+            //         value:6,
+            //         name:"灯箱5"
+            //     }
+            //     ];
+
+            $scope.data = data;
+        }
+
+
+    }
+    //搜索跳转
+    $scope.search = function(obj){
+        //$state.go('app.assetmanage.fixedassetmanage',{search:$scope.search_context});
+        //alert("$stateParams.page"+$stateParams.page);
+        //alert("$stateParams.search"+obj.search);
+        $scope.query(1,obj.search);
+    }
+    //分页跳转
+    $scope.paging = function(obj){
+        //$state.go('app.assetmanage.fixedassetmanage',{search:$scope.search_context});
+        //alert("$stateParams.page"+page.page);
+        //alert("$stateParams.search"+page.search);
+        $scope.query(obj.page,obj.search);
     }
     //根据url参数（分页、搜索关键字）查询数据
     $scope.query($stateParams.page,$stateParams.search);
